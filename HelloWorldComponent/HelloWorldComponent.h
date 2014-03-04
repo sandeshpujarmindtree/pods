972db7ef316d14b53fe6e4545f5c35847d022195
sandeshpujarmindtree <sandesh_pujar@mindtree.com>
2014-03-04 22:54:46 +0530

    HelloWorldComponent codebase

---
+++ HelloWorldComponent/HelloWorldComponent.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorldComponent.h
+//  AProject
+//
+//  Created by Sandesh Pujar on 04/03/14.
+//  Copyright (c) 2014 Sandesh Pujar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HelloWorldComponent : NSObject
+
+- (NSString *)returnHelloWorld;
+
+@end
---
+++ HelloWorldComponent/HelloWorldComponent.m
@@ -0,0 +1,18 @@
+//
+//  HelloWorldComponent.m
+//  AProject
+//
+//  Created by Sandesh Pujar on 04/03/14.
+//  Copyright (c) 2014 Sandesh Pujar. All rights reserved.
+//
+
+#import "HelloWorldComponent.h"
+
+@implementation HelloWorldComponent
+
+- (NSString *)returnHelloWorld
+{
+    return @"Hello World";
+}
+
+@end
